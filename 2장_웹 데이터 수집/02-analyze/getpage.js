4011e16814038c98f439b89188b888088799d936
jung <jnoah80@gmail.com>
2017-02-23 23:36:22 +0900

    jung

---
+++ 2장_웹 데이터 수집/02-analyze/getpage.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017-02-22.♣♣jung♣♣
+ */
+var client = require('~/.nvm/vertion/cheerio-httpcli');
+
+var url = "http://jpub.tistory.com";
+var param = {};
+
+client.fetch(url, param, function(err, $, res){
+    if (err){
+        console.log("Error : ", err); return;
+    }
+    var body = $.html();
+    console.log(body);
+});
