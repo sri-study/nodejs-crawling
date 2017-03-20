0c56551d58a523a0cce9ed186e03ec1251cfa181
eggplant <jjp372c@naver.com>
2017-03-20 15:24:24 +0900

    코드 수정

--- 4장_데이터 처리 및 저장/ch04/05-db/sqlite-book.js
+++ 4장_데이터 처리 및 저장/ch04/05-db/sqlite-book.js
@@ -7,7 +7,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(DB_PATH);
 
 var BASE_URL = 'http://jpub.tistory.com/category/' + encodeURIComponent('제이펍의 도서');
-var PAGE_NUM = 6;
+var PAGE_NUM = 2; // 기간 관계상 2페이지 까지만
 
 var client = require('cheerio-httpcli');
 var fs = require('fs');
@@ -43,16 +43,19 @@ function scrape(page) {
 
 function dbinsert() {
     db.serialize(function () {
+        // 기존 테이블 테이터가 잘못들어가서 삭제
+        db.run('DROP TABLE IF EXISTS book');
         db.run('CREATE TABLE IF NOT EXISTS book(' +
-            'id INTEGER PRIMARY KEY,' +
+            'id INTEGER PRIMARY KEY, ' +
             'token TEXT)');
 
         var ins_stmt = db.prepare(
-            'INSERT INTO book (token)' +
+            'INSERT INTO book (token) ' +
             'VALUES(?)');
 
         booklist.forEach(function (value, index, array) {
-            var words = value.split('');
+            // 공백기준으로 배열을 만들어야 하는데 오타가 있어서 수정.
+            var words = value.split(' ');
 
             for (var i in words) {
                 ins_stmt.run(words[i]);
@@ -61,8 +64,10 @@ function dbinsert() {
         ins_stmt.finalize();
 
         console.log('집계결과');
-        db.each('SELECT token, COUNT(token) as cnt' +
-            'FROM book GROUP BY token having cnt > 3' +
+
+        // 쿼리문이 뛰어쓰기가 없어서 수정
+        db.each('SELECT token, COUNT(token) as cnt ' +
+            'FROM book GROUP BY token having cnt > 3 ' +
             'ORDER BY cnt DESC',
         function (err, row) {
             if (err) {console.log('CNT error'); return;}
