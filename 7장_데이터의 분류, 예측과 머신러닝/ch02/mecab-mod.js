85c27f5975f48dc1d57c8ef695e0a30800320eb9
박동형 <dreams402@saramin.co.kr>
2017-05-25 10:58:37 +0900

    - 7장 챕터2,3 소스 push

---
+++ 7장_데이터의 분류, 예측과 머신러닝/ch02/mecab-mod.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+	var execSync = require('child_process').execSync;
+	var fs = require('fs');
+
+	this.parse = function(text, callback) {
+		text += "\n";
+
+		fs.writeFileSync('TMP_INPUT_FILE', text, "UTF-8");
+
+		var cmd = [
+			'mecab',
+			'TMP_INPUT_FILE',
+			'--output=TMP_OUTPUT_FILE'
+		].join(" ");
+
+		var opt = { encoding : 'UTF-8' };
+		var res = [];
+		try {
+			execSync(cmd, opt);
+			res = fs.readFileSync('TMP_OUTPUT_FILE', 'UTF-8');
+		} catch (e) {
+			console.log(e);
+		}
+
+		res = res.replace(/\r/g, "");
+		res = res.replace(/\s+$/, "");
+		var lines = res.split("\n");
+
+		var res = lines.map(function(line) {
+			return line.replace('\t', ',').split(',');
+		});
+
+		callback(res);
+	}
+}
